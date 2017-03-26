5fba893fb50678079dc776d7ab38e92c4c22ff1b
ztaihong <zth@xjau.edu.cn>
2017-03-27 02:36:16 +0800

    淘宝第三方认证将用户昵称有url编码转化为UTF-8编码

--- allauth/socialaccount/providers/taobao/provider.py
+++ allauth/socialaccount/providers/taobao/provider.py
@@ -1,11 +1,10 @@
 from allauth.socialaccount.providers.base import ProviderAccount
 from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
 from django.utils.translation import ugettext_lazy as _
+from urllib.parse import unquote  # url解码
+
 
 class TaobaoAccount(ProviderAccount):
-
-
-
     def to_str(self):
         return self.account.extra_data.get(
             'uname',
@@ -18,17 +17,17 @@ class TaobaoProvider(OAuth2Provider):
     account_class = TaobaoAccount
 
     def extract_uid(self, data):
-
         # return data.get('taobao_user_id')
         return data['response']['taobao_user_id']
 
-
     def extract_common_fields(self, data):
         uid = data['response']['taobao_user_id']
-        nick  = data['response']['taobao_user_nick']
+        nick = data['response']['taobao_user_nick']
         uname = data['response']['taobao_user_nick']
-        #return dict(uid=data.get('taobao_user_id'), username=data.get('taobao_user_nick'), uname=data.get('taobao_user_nick'))
-        return dict(uid = data['response']['taobao_user_id'],
-                    username = data['response']['taobao_user_nick'], uname=data['response']['taobao_user_nick'])
+        # return dict(uid=data.get('taobao_user_id'), username=data.get('taobao_user_nick'), uname=data.get('taobao_user_nick'))
+        return dict(uid=data['response']['taobao_user_id'],
+                    username=unquote(data['response']['taobao_user_nick']),
+                    uname=unquote(data['response']['taobao_user_nick']))
+
 
 provider_classes = [TaobaoProvider]
