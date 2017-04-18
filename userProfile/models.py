153804563cc29c11fbe38b272bfa52510fde976c
ztaihong <zth@xjau.edu.cn>
2017-04-18 23:33:28 +0800

    个人资料

--- userProfile/models.py
+++ userProfile/models.py
@@ -4,10 +4,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Languages(models.Model):
+    """
+    zh-hans, 简体中文
+    en-us,   English
+    ug-ar,   ئۇيغۇر تىلى‎
+    kk-ar,   قازاق ٴتىلى
+    """
+
+    localeCode =  models.CharField(max_length=10, primary_key=True)   # 语言代码
+    languageName = models.TextField(max_length=30, blank=False)       # 语言名称
+
+
 class UserProfile(models.Model):
-    user = models.ForeignKey(User, unique=True)
-    userName = models.CharField(max_length=30, blank=True)
-    
+    user = models.ForeignKey(User, unique=True)                        # 用户ID
+    userName = models.CharField(max_length=30, blank=True)             # 真实姓名
+    localeCode = models.ForeignKey(Languages)                          # 偏好语言代码
+    mobile = models.CharField(max_length=15)                           # 手机号码
+    mobileVerified = models.BooleanField(default=False, blank=False)   # 手机号码是否通过短信验证
+    address = models.TextField(max_length=200)                         # 通讯地址
+
 
 
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
